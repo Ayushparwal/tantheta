913d04532ac3858496c754654406908ea4f717f0
Ayushparwal <parwalayush5@gmail.com>
2025-07-20 20:12:22 +0530

    version

--- tantheta/__init__.py
+++ tantheta/__init__.py
@@ -1,6 +1,6 @@
 from .core import add, subtract, multiply, divide
-from tantheta.algebra import dot_product, cross_product,classify_conic
-from tantheta.calculus import differentiate, integration, find_limit
-from tantheta.probability import nCr, nPr, basic_probability
-from tantheta.stats import mean, median, standard_deviation, variance
-from tantheta.trigonometry import solve_trig_equation
+from .algebra import dot_product, cross_product, classify_conic
+from .calculus import differentiate, integration, find_limit
+from .probability import nCr, nPr, basic_probability
+from .stats import mean, median, standard_deviation, variance
+from .trigonometry import solve_trig_equation
