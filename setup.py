153b502837a28a5b8bbca95aa214ff06fcd60def
Ayushparwal <parwalayush5@gmail.com>
2025-07-20 20:28:43 +0530

    set

--- setup.py
+++ setup.py
@@ -9,8 +9,24 @@ try:
 except Exception:
     long_description = ""
 
-
 setup(
-    packages=find_packages(),
+    name="tantheta",  # ✅ required
+    version="1.0.3",  # ✅ increment this with each upload
+    author="Ayush Parwal",
+    author_email="ayushparwal777@gmail.com",  # optional
+    description="A symbolic math toolkit for algebra, calculus, etc.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ayushparwal/tantheta",  # ✅ update if public
+    packages=find_packages(),  # this will include tantheta/*
     include_package_data=True,
+    install_requires=[
+        "sympy>=1.12",  # ✅ or other dependencies
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
 )
